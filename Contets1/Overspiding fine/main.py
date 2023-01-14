a8cc508a61cc438e9cec2c359855175996cef676
sachin <007mishrasachinmishra@gmail.com>
2023-01-14 23:10:32 +0530

    first commit

---
+++ Contets1/Overspiding fine/main.py
@@ -0,0 +1,12 @@
+# cook your dish here
+tes = int(input())
+i=int()
+x =int(input())
+for i in range(tes):
+    if x <= 70:
+        print("0")
+    elif 70< x <= 100:
+        print("500")
+    else:
+        print("2000")
+    
